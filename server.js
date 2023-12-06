d0c489845d365d094701b26cf711cb7abcbd2421
Travis Fowlston <travisfowlston@ymail.com>
2023-12-05 20:19:25 -0800

    added a server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
