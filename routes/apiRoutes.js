9d7bb74640b82f991deb6856d58d359d22980212
Travis Fowlston <travisfowlston@ymail.com>
2024-02-03 22:22:50 -0800

    finished the apiRoutes file and updated code with comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,55 @@
 const router = require("express").Router();
-const { notes } = require("../db/db.json");
+const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+
+const readFromFile = fs.promises.readFile;
+const writeToFile = fs.promises.writeFile;
+
+// This is the route that will read the db.json file and will return all the saved notes as JSON.
+router.get("/notes", async (req, res) => {
+  try {
+    const notes = await readFromFile("./db/db.json");
+    const parseTheNotes = JSON.parse(notes);
+    res.json(parseTheNotes);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// This is the route that will add a new note to the db.json file.
+router.post("/notes", async (req, res) => {
+  try {
+    const notes = await readFromFile("./db/db.json");
+    const parseTheNotes = JSON.parse(notes);
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4(),
+    };
+    parseTheNotes.push(newNote);
+    const updatedNotes = await writeToFile(
+      "./db/db.json",
+      JSON.stringify(parseTheNotes)
+    );
+    res.json(updatedNotes);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// This is the route that will delete a note based on the id of that note.
+router.delete("/notes/:id", async (req, res) => {
+  try {
+    const notes = await readFromFile("./db/db.json");
+    const parseTheNotes = JSON.parse(notes);
+    const updatedNotes = parseTheNotes.filter(
+      (note) => note.id !== req.params.id
+    );
+    await writeToFile("./db/db.json", JSON.stringify(updatedNotes));
+    res.json(updatedNotes);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("*", (req, res) => {
+// This is the route that will send the user to the index.html file.
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
+// This is the route that will send the user to the notes.html file.
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
